b40099c1e12833f27fad82954c8f551c6e147d9c
DanielRomero <danielromero@mail.weber.edu>
2016-04-18 13:41:35 -0600

    Updated table to use JQuery data tables

--- TestGraphZoom.js
+++ TestGraphZoom.js
@@ -3,11 +3,9 @@ var rightBound = 4.00;
 
 var leftScale = 0.00;
 var rightScale = 4.00;
-<<<<<<< HEAD
-var graphObjectArray =  new Array();
-=======
+
 var graphObjectArray = [];
->>>>>>> origin/master
+
 var tableSort = "";
 var tableData = null;
 
@@ -82,44 +80,53 @@ function startGraph()
 	//var graphObjectArray = new Array();//[graphObject, graphObject2, graphObject3, graphObject4, graphObject5, graphObject6, graphObject7, graphObject8, graphObject9];
 	
 	//generate random markers to use as mock data. 
-	 for (var i = 0; i < 200; i++)
-	 {
-		 var tempScore = (Math.random() * 4);
-		 var tempMarker;
-		
-		 var tempColor = (Math.random() * 50).toFixed(0);
+	for (var i = 0; i < 10; i++)
+	{
+		var tempScore = (Math.random() * 4).toFixed(2);
+		var tempMarker;
 
-		 var semester = Math.floor((Math.random() * 3)) + 1;
-		 var year = Math.floor((Math.random() * 5)) + 2013;
-		
-		 if (tempColor == 0)
-		 {
-			 tempMarker = "red";
-		 }
-		 else if (tempColor == 1)
-		 {
-			 tempMarker = "green";
-		 }
-		 else if (tempColor >= 2)
-		 {
-			 tempMarker = "blue";
-		 }
-		
+		var tempColor = (Math.random() * 50).toFixed(0);
 
-		 //assign info to an object with mock data
-		 var tempObject = {marker:tempMarker, course:"CS 1400", instructor:"Brad Peterson", score:tempScore, year:year, semester: semester};
-		 graphObjectArray.push(tempObject);
-	 }
-	
+		var semester = function randSemester(){
+			var x = Math.floor((Math.random() * 3)) + 1;
+			if(x == 1){
+				return "Spring";
+			}
+			else if(x == 2 ){
+				return "Summer";
+			}
+			else{
+				return "Fall";
+			}
+		}
+		var year = Math.floor((Math.random() * 5)) + 2013;
+
+		if (tempColor == 0)
+		{
+			tempMarker = "red";
+		}
+		else if (tempColor == 1)
+		{
+			tempMarker = "green";
+		}
+		else if (tempColor >= 2)
+		{
+			tempMarker = "blue";
+		}
+
+
+		//assign info to an object with mock data
+		var tempObject = {marker:tempMarker, course:"CS 1400", instructor:"Brad Peterson", score:tempScore, year:year, semester: semester};
+		graphObjectArray.push(tempObject);
+	}
+
 	graphObjectArray.sort(function (a, b) {
-	    return a.score - b.score
+		return a.score - b.score
 	})
 	tableData = graphObjectArray;
 	barGraph();
-
-	generateScoreTable(tableData);
-	
-
+	objectToArr(tableData);
+	//generateScoreTable(tableData);
 }
 
 
@@ -563,119 +570,163 @@ function nearestHalf(value)
 }
 
 
-function generateScoreTable(scoreArray)
-{
-    var table = document.getElementById('tabularScores');
-    table.innerHTML = "";
-    
-
-    if(!table)
-    {
-        return;
-    }
-
-    
-    var html = "";
-
-    html += "<tr>";
-    html += "<th class='sortableHeader' onclick='setTimeout(0,sortKey());'>Key</th>"
-    html += "<th class='sortableHeader' onclick='setTimeout(0,sortClassName());'>Class</th>";
-    html += "<th class='sortableHeader' onclick='setTimeout(0,sortInstructor());'>Instructor</th>";
-    html += "<th class='sortableHeader' onclick='setTimeout(0,sortSemester());'>Semester</th>";
-    html += "<th class='sortableHeader' onclick='setTimeout(0,sortScore());'>Score</th>";
-    html += "</tr>";
-
-    for (var i = 0; i < scoreArray.length; i++)
-    {
-        var semester = "";
-        var semesterNum = parseInt(scoreArray[i].semester);
-        year = parseInt(scoreArray[i].year);
-        switch (semesterNum) {
-            case 1:
-                semester = "Summer";
-                year--;
-                break;
-            case 2:
-                semester = "Fall";
-                year--;
-                break;
-            case 3:
-                semester = "Spring";
-                break;
-        }
-
-
-        html += "<tr>";
-        html += "<td style=background-color:" + scoreArray[i].marker +"></td>";
-        html += "<td>" + scoreArray[i].course + "</td>";
-        html += "<td>" + scoreArray[i].instructor + "</td>";
-        html += "<td>" + semester + " " + year + "</td>";
-        html += "<td>" + scoreArray[i].score.toFixed(2) + "</td>";
-    }
-
-
-
-    table.innerHTML += html;
-        
+function objectToArr(tableValues) {
+	var dataSet = [];
+	for (i = 0; i < 10; i++) {
+		dataSet[i] = [tableValues[i].marker, tableValues[i].course,tableValues[i].instructor,tableValues[i].semester,tableValues[i].year,tableValues[i].score];
+	}
+	buildTable(dataSet);
 }
 
-function sortScore()
-{
-    tableData.sort(function (a, b) {
-
-        return b.score - a.score;
-    });
-
-    generateScoreTable(tableData);
+//Builds table using JQuery DataTables
+function buildTable(dataSet){
+	$('#tabularScores').DataTable( {
+		"lengthChange": false,
+		"searching": false,
+		"paging": false,
+		"order": [[ 5, "desc" ]],
+		data: dataSet,
+		"columnDefs": [ {
+			"targets": 0,
+			"createdCell": function (td, cellData) {
+				if ( cellData == "blue" ) {
+					$(td).css('color', 'blue');
+					$(td).css('background-color', 'blue');
+				}
+				if ( cellData == "green" ) {
+					$(td).css('color', 'green');
+					$(td).css('background-color', 'green');
+				}
+				if ( cellData == "red" ) {
+					$(td).css('color', 'red');
+					$(td).css('background-color', 'red');
+				}
+			}
+		}],
+		columns: [
+			{ title: "Key" },
+			{ title: "Class" },
+			{ title: "Instructor" },
+			{ title: "Semester" },
+			{ title: "Year"},
+			{ title: "Score" }
+		]
+	} );
 }
 
-function sortInstructor()
-{
-    tableData.sort(function (a, b) {
-
-        if (a.instructor < b.instructor)
-            return -1;
-        if (a.instructor > b.instructor)
-            return 1;
-        else
-            return 0;
-    });
-
-    generateScoreTable(tableData);
-}
-
-function sortSemester()
-{
-
-    tableData.sort(function (a, b) {
-
-       
-        return parseInt("" + a.year + a.semester) - parseInt("" + b.year + b.semester);
-    });
-
-    generateScoreTable(tableData);
-
-}
-function sortClassName()
-{
-    tableData.sort(function (a, b) {
-
-        if (a.course < b.course)
-            return -1;
-        if (a.course > b.course)
-            return 1;
-        else
-            return 0;
-        
-    });
-    generateScoreTable(tableData);
-}
-
-function sortKey()
-{
-    tableData.sort(function (a, b) {
-        //we'll use getcolorzindex to help determine order when sorting by key
-        return getColorZIndex(b.marker) - getColorZIndex(a.marker);
-    });
-    generateScoreTable(tableData);
-}
+//function generateScoreTable(scoreArray)
+//{
+//    var table = document.getElementById('tabularScores');
+//    table.innerHTML = "";
+//
+//
+//    if(!table)
+//    {
+//        return;
+//    }
+//
+//
+//    var html = "";
+//
+//    html += "<tr>";
+//    html += "<th class='sortableHeader' onclick='setTimeout(0,sortKey());'>Key</th>"
+//    html += "<th class='sortableHeader' onclick='setTimeout(0,sortClassName());'>Class</th>";
+//    html += "<th class='sortableHeader' onclick='setTimeout(0,sortInstructor());'>Instructor</th>";
+//    html += "<th class='sortableHeader' onclick='setTimeout(0,sortSemester());'>Semester</th>";
+//    html += "<th class='sortableHeader' onclick='setTimeout(0,sortScore());'>Score</th>";
+//    html += "</tr>";
+//
+//    for (var i = 0; i < scoreArray.length; i++)
+//    {
+//        var semester = "";
+//        var semesterNum = parseInt(scoreArray[i].semester);
+//        year = parseInt(scoreArray[i].year);
+//        switch (semesterNum) {
+//            case 1:
+//                semester = "Summer";
+//                year--;
+//                break;
+//            case 2:
+//                semester = "Fall";
+//                year--;
+//                break;
+//            case 3:
+//                semester = "Spring";
+//                break;
+//        }
+//
+//
+//        html += "<tr>";
+//        html += "<td style=background-color:" + scoreArray[i].marker +"></td>";
+//        html += "<td>" + scoreArray[i].course + "</td>";
+//        html += "<td>" + scoreArray[i].instructor + "</td>";
+//        html += "<td>" + semester + " " + year + "</td>";
+//        html += "<td>" + scoreArray[i].score.toFixed(2) + "</td>";
+//    }
+//
+//
+//
+//    table.innerHTML += html;
+//
+//}
+//
+//function sortScore()
+//{
+//    tableData.sort(function (a, b) {
+//
+//        return b.score - a.score;
+//    });
+//
+//    generateScoreTable(tableData);
+//}
+//
+//function sortInstructor()
+//{
+//    tableData.sort(function (a, b) {
+//
+//        if (a.instructor < b.instructor)
+//            return -1;
+//        if (a.instructor > b.instructor)
+//            return 1;
+//        else
+//            return 0;
+//    });
+//
+//    generateScoreTable(tableData);
+//}
+//
+//function sortSemester()
+//{
+//
+//    tableData.sort(function (a, b) {
+//
+//
+//        return parseInt("" + a.year + a.semester) - parseInt("" + b.year + b.semester);
+//    });
+//
+//    generateScoreTable(tableData);
+//
+//}
+//function sortClassName()
+//{
+//    tableData.sort(function (a, b) {
+//
+//        if (a.course < b.course)
+//            return -1;
+//        if (a.course > b.course)
+//            return 1;
+//        else
+//            return 0;
+//
+//    });
+//    generateScoreTable(tableData);
+//}
+//
+//function sortKey()
+//{
+//    tableData.sort(function (a, b) {
+//        //we'll use getcolorzindex to help determine order when sorting by key
+//        return getColorZIndex(b.marker) - getColorZIndex(a.marker);
+//    });
+//    generateScoreTable(tableData);
+//}
